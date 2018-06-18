bd60c23af3002e640365452e486d0c29cd72015e
Jonne Kanerva <jonne.kanerva@siili.com>
2018-06-18 11:53:09 +0300

    Tehtävä 2.6

---
+++ 2.6-2.19/src/App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+const Puhelinluettelo = (props) => {
+  const persons = props.persons.map( (person, i) => {
+    return <Person key={person.name} person={person} />
+  })
+  return (
+    <ul>
+      {persons}
+    </ul>
+  )
+}
+
+const Person = (props) => {
+  return (
+    <li>
+      {props.person.name}
+    </li>
+  )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  handleNameChange = event => {
+    this.setState({
+      newName: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const newPerson = {
+      name: this.state.newName
+    }
+    const persons = this.state.persons.concat(newPerson)
+
+    this.setState({
+      persons: persons
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.handleSubmit} >
+          <div>
+            nimi:
+            <input
+              onChange={this.handleNameChange}
+              value={this.state.newName} />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        <Puhelinluettelo persons={this.state.persons} />
+      </div>
+    )
+  }
+}
+
+export default App
