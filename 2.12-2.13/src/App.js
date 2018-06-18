5f2e8005d618946863f7fa0b828f270ecbc5343a
Jonne Kanerva <jonne.kanerva@siili.com>
2018-06-18 14:51:16 +0300

    Tehtävä 2.12

---
+++ 2.12-2.13/src/App.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import axios from 'axios'
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      countries: [],
+      filter: ''
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://restcountries.eu/rest/v2/all').then( response => {
+      this.setState({
+        countries: response.data
+      })
+    })
+  }
+
+  handleChange = event => {
+    this.setState({
+      filter: event.target.value
+    })
+  }
+
+  render() {
+
+    const filteredCountries = this.state.countries.filter( country => {
+      return country.name.toLowerCase().indexOf(this.state.filter) > -1
+    })
+
+    const countries = filteredCountries.map( country => {
+      return <li key={country.name}>{country.name}</li>
+    })
+
+    return (
+      <div>
+        find countries:
+        <input
+          onChange={this.handleChange}
+          value={this.state.filter} />
+
+        {filteredCountries &&
+         filteredCountries.length === 1 ?
+          <div>
+            <h1>{filteredCountries[0].name}</h1>
+            <p>capital: {filteredCountries[0].capital}</p>
+            <p>population: {filteredCountries[0].population}</p>
+            <img src={filteredCountries[0].flag} />
+          </div> :
+          null
+        }
+
+        {countries &&
+         countries.length > 1 &&
+         countries.length <= 10 ?
+          <ul> {countries} </ul> :
+          null
+        }
+
+        {countries &&
+         countries.length > 10 ?
+          <p>too many matches, specify another filter</p> :
+          null
+        }
+      </div>
+    )
+  }
+}
+
+export default App;
